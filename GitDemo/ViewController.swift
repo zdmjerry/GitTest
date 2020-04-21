996e4d4af578ceca6973c758f034469956d0079e
zhengdingming <zhengdingming@camera360.com>
2020-04-21 10:42:05 +0800

    first commit

---
+++ GitDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GitDemo
+//
+//  Created by dingming on 2020/4/21.
+//  Copyright © 2020 jerry. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
