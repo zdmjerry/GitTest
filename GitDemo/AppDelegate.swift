07dee65a587ee269f4ca9fd3cbd5c70146bd03cc
zhengdingming <zhengdingming@sm.vvip-u.com>
2020-06-04 11:28:05 +0800

    rebase3

--- GitDemo/AppDelegate.swift
+++ GitDemo/AppDelegate.swift
@@ -16,8 +16,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         print("application")
-         print("application")
-        print("application")
         return true
     }
 
